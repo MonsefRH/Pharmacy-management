491afea3c790969e1079c0667dcc6c53d451a8a9
nadabouziane <161883878+nadabouziane@users.noreply.github.com>
2025-01-01 22:12:19 +0100

    Update AddUserController.java

--- src/main/java/org/example/Controllers/AddUserController.java
+++ src/main/java/org/example/Controllers/AddUserController.java
@@ -13,7 +13,7 @@ public class AddUserController {
         addUserModel = new AddUserModel();
     }
 
-    // This method will handle the saving of user information
+    // This method will handle the saving of user info
     public void saveUser(String role, String fullName, String dob, String phone, String email, String username, String password, String address) {
         boolean isSaved = addUserModel.saveUserToDatabase(role, fullName, dob, phone, email, username, password, address);
 

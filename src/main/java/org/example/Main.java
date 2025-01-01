37afd2f68da66b7261de6a93c9b97390c352c8db
nadabouziane <161883878+nadabouziane@users.noreply.github.com>
2025-01-01 21:47:06 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,1 @@
-package org.example;
 
-import org.example.View.AddUserView;
-
-import javax.swing.SwingUtilities;
-
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new AddUserView().setVisible(true));
-    }
-}
